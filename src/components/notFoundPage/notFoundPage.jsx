c9d65f0afa4956ea21d6d5759a769c24d1f17bde
NadyaGus <ftotkexit@gmail.com>
2024-09-01 20:24:00 +0300

    feat: change error page

--- src/components/notFoundPage/notFoundPage.jsx
+++ src/components/notFoundPage/notFoundPage.jsx
@@ -1,7 +1,12 @@
+import { Container } from "@mantine/core";
+import { NavLink } from "react-router-dom";
+
 export const NotFoundPage = () => {
   return (
-    <div>
+    <Container>
       <h1>Page Not Found</h1>
-    </div>
+      <p>The page you are looking for does not exist.</p>
+      <NavLink to="/">Go Back</NavLink>
+    </Container>
   );
 };
